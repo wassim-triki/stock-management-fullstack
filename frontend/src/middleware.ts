5337ba40199e407d207864041e8208b1134aaa26
Wassim Triki <wsmtriki@gmail.com>
2024-08-18 06:57:57 +0100

    fix server down error

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,22 +1,37 @@
+//middleware.ts
 import { NextRequest, NextResponse } from "next/server";
 import fetchHelper from "./lib/fetchInstance";
 import { ApiSuccessResponse, User } from "./lib/types";
 import { cookies } from "next/headers";
 import appConfig from "@/lib/config";
+
 const middleware = async (req: NextRequest) => {
   const { pathname } = req.nextUrl;
   const cookieStore = cookies();
   const cookieValue = cookieStore.get("session")?.value;
-  const response = await fetch(`${appConfig.apiUrl}/api/auth/me`, {
-    credentials: "include", // Ensure credentials are sent
-    headers: {
-      "Content-Type": "application/json",
-      Cookie: "session=" + cookieValue,
-    },
-  });
 
-  const isAuthenticated = response.ok;
+  let isAuthenticated = false; // Default assumption is that the user is not authenticated
 
+  try {
+    // Try fetching the authentication status from the API
+    const response = await fetch(`${appConfig.apiUrl}/api/auth/me`, {
+      credentials: "include", // Ensure credentials are sent
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: "session=" + cookieValue,
+      },
+    });
+
+    // Check if the user is authenticated
+    isAuthenticated = response.ok;
+  } catch (error) {
+    console.error("Error fetching authentication status:", error);
+
+    // Optionally, you could redirect the user to a maintenance page if the server is down
+    return NextResponse.redirect(new URL("/maintenance", req.url));
+  }
+
+  // Handle redirection based on authentication status
   if (pathname.startsWith("/dashboard") && !isAuthenticated) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
